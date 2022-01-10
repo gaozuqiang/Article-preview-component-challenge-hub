d6d1cde19684ff08f89ef2ccf0bbbdcd66f5de40
gaozuqiang <68024818+gaozuqiang@users.noreply.github.com>
2022-01-10 00:36:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function js_style(){
+  var x = document.getElementById("icons");
+  if (x.style.display === "none") {
+    x.style.display = "inline-block";
+  } else {
+    x.style.display = "none";
+  }
+}
